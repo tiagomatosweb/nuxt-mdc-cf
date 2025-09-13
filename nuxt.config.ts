1d0fd85d331af8084df821d829698737e6d5e45c
Tiago Matos <tiago@tiagomatos.com>
2025-09-13 13:23:27 +1000

    Test MDC

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr:false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/mdc'],
